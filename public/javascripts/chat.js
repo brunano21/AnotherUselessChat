86b3c222cadeca103a7d1a070bdf03628c0b2ca4
bR1 <brunano21@gmail.com>
2014-10-14 10:43:56 +0200

    CSS rewritten 2, added function on chat'sendButton

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,8 +2,8 @@ window.onload = function() {
 
     var chatLog = [];
     var socket = io.connect('http://localhost:3000');
-    var field = $("#field");
-    var sendButton = $("#send");
+    var inputText = $("#inputText");
+    var sendBtn = $("#sendBtn");
     var content = $("#content");
     var signInBtn = $("#signInBtn");
 
@@ -21,15 +21,30 @@ window.onload = function() {
         }
     });
 
-    sendButton.onclick = function() {
-        var text = field.value;
+    sendBtn.on('click', function() {
+        var text = inputText.val();
+        
         socket.emit('sendMsg', { message: text });
+        /*TODO: bisogna capire se ha senso inviare il messaggio al server e quando questo mi risponde con un ack allora stampo il messaggio 
+        oppure se lo attacco direttamente, cosi come viene fatto per ora. */
         console.log(text);
-    };
+        
+        $("#chat-container > ul").append(
+        '<li>' +
+            '<div class="bubble2">' +
+              '<span class="personName2"> '+ 'Nick' + '<span>' +
+              '<span class="personSay2"> '+ text + '<span>' +
+              '<span class="time2 round"> '+ '12.55 AM' + '<span>' +
+        '</li>');
+        $("#chat-container").animate({scrollTop: $("#chat-container")[0].scrollHeight}, 600);
+        
+    });
 
     // momentanea
     //$("#signInModal").modal('toggle');
     /*$("#signUpBtn").onclick = function() {
         ('toggle');
     }*/
+
+
 }
